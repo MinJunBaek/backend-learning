c3677d7cd67b47e8de0fbcb6ef6fd90a154aa4a0
Mac_BaekMinJun <baekminjun92@gmail.com>
2024-11-11 15:43:55 +0900

    [add] 7-1-5완료

---
+++ course1/src/fc/java/course2/part3/WeatherExample.java
@@ -0,0 +1,47 @@
+package fc.java.course2.part3;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WeatherExample {
+    public static void main(String[] args) {
+        String apiKey = "MyAPI Key";
+        String city = "Seoul";
+        String urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+            int responseCode = connection.getResponseCode(); // 200이 정상
+            if (responseCode == 200) {
+               // 스트림(입력, 출력)의 연결
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+                System.out.println("content.toString() = " + content.toString());
+                JsonObject weatherData = JsonParser.parseString(content.toString()).getAsJsonObject();
+                JsonObject mainData = weatherData.getAsJsonObject("main");
+                double temp = mainData.get("temp").getAsDouble();
+                System.out.println("temp = " + temp);
+                connection.disconnect();
+            } else {
+                //오류가 나면 else구문 실행
+                System.out.println(responseCode);
+                System.out.println("오류");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
